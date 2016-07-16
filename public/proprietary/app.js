3ef8c49f2523839493a1582e81dda6b6ad09adce
Craig Ceremuga <cceremuga@nimbleuser.com>
2016-07-16 11:15:42 -0400

    maps

--- public/proprietary/app.js
+++ public/proprietary/app.js
@@ -1,5 +1,19 @@
+var map;
+
 $(document).ready(function() {
 	var windowHeight = window.innerHeight;
-	console.log(windowHeight);
+
+	// Size map.
+
+	// Size post box.
 	$('#posts-by-date').height(windowHeight - 260 + 'px');
+	$('#posts-by-location').height(windowHeight - 212 + 'px');
 });
+
+      
+function initMap() {
+	map = new google.maps.Map(document.getElementById('posts-by-location'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
