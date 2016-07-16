91821e3526d3af2071154ec3ceb91b4982624c4f
Craig Ceremuga <cceremuga@nimbleuser.com>
2016-07-16 08:52:50 -0400

    Incorporation of lightning design design system

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var cool = require('cool-ascii-faces')
 var express = require('express');
 var app = express();
 var pg = require('pg');
 
+// Set listening port.
 app.set('port', (process.env.PORT || 5000));
 
+// Set directory for static resources.
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// Master templates.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Index page.
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// Db test page.
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
@@ -27,8 +30,7 @@ app.get('/db', function (request, response) {
   });
 });
 
+// Start server.
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
